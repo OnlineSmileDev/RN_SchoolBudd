9735fe8e96b6525bdb71a47cc78c39e6c5213824
MobileWorldev127 <alexander.soft@hotmail.com>
2019-01-19 23:15:00 +0200

    change goalstimeline screen

--- app/pages/goalsTimeline.js
+++ app/pages/goalsTimeline.js
@@ -48,17 +48,6 @@ export default class Home extends React.Component {
     return (
       <View style={styles.container} >
 
-        {/* <TouchableOpacity onPress={() => this.navigateFreshmanYear()}>
-          <LinearGradient
-            start={{x: 0, y: 0}} end={{x: 1, y: 0}}
-            colors={['#CE9FFC', '#7367F0']}
-            style={styles.yearItem}>
-            <Text style={styles.btnTxt}>
-              Freshman Year
-            </Text>
-          </LinearGradient>
-        </TouchableOpacity> */}
-
         <TouchableOpacity onPress={() => this.navigateFreshmanYear()}>
             <LinearGradient
               start={{x: 0, y: 0}} end={{x: 1, y: 0}}
@@ -87,31 +76,33 @@ export default class Home extends React.Component {
             </LinearGradient>
         </TouchableOpacity> 
 
-        <TouchableOpacity onPress={() => this.navigateSophomoreYear()}>
+        <TouchableOpacity onPress={() => this.navigateJuniorYear()}>
             <LinearGradient
               start={{x: 0, y: 0}} end={{x: 1, y: 0}}
-              colors={['#FEB692', '#EA5455']}
+              colors={['#90F7EC', '#32CCBC']}
               style={styles.yearItem}>
-              <View style = {[styles.circleView, {borderColor: '#FEB692'}]}>
-                <Text style = {[styles.numberTxt, {color: '#FEB692'} ]}>2</Text>
+              <View style = {[styles.circleView, {borderColor: '#90F7EC'}]}>
+                <Text style = {[styles.numberTxt, {color: '#90F7EC'} ]}>3</Text>
               </View>
               <Text style={styles.btnTxt}>
-                Sophomore Year
+                Junior Year
+              </Text>
+            </LinearGradient>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => this.navigateSeniorYear()}>
+            <LinearGradient
+              start={{x: 0, y: 0}} end={{x: 1, y: 0}}
+              colors={['#FCCF31', '#F55555']}
+              style={styles.yearItem}>
+              <View style = {[styles.circleView, {borderColor: '#FCCF31'}]}>
+                <Text style = {[styles.numberTxt, {color: '#FCCF31'} ]}>4</Text>
+              </View>
+              <Text style={styles.btnTxt}>
+                Senior Year
               </Text>
             </LinearGradient>
         </TouchableOpacity> 
-
-        <Button
-          buttonStyle={ styles.yearItem }
-          onPress={() => this.navigateJuniorYear()}
-          title='Junior Year'>
-        </Button>
-
-        <Button
-          buttonStyle={ styles.yearItem }
-          onPress={() => this.navigateSeniorYear()}
-          title='Senior Year'>
-        </Button>
      </View>
     );
   }
